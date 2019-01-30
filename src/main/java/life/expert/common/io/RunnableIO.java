72e96742d035688f47ad0ee526c46cbe9d04cf7f
wilmer krisp <wilmerkrisp@yahoo.com>
2019-01-30 15:42:28 +0300

    io helper classes

---
+++ src/main/java/life/expert/common/io/RunnableIO.java
@@ -0,0 +1,52 @@
+package life.expert.common.io;
+//@Header@
+//--------------------------------------------------------------------------------
+//
+//                          archidoc  life.expert
+//                           wilmer 2019/01/30
+//
+//--------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+import java.io.IOException;
+
+
+
+
+
+
+
+
+
+/**
+ * The interface Runnable io.
+ */
+@FunctionalInterface
+public interface RunnableIO
+	{
+	
+	
+	
+	/**
+	 * When an object implementing interface <code>Runnable</code> is used
+	 * to create a thread, starting the thread causes the object's
+	 * <code>run</code> method to be called in that separately executing
+	 * thread.
+	 * <p>
+	 * The general contract of the method <code>run</code> is that it may
+	 * take any action whatsoever.
+	 *
+	 * @throws IOException
+	 * 	the io exception
+	 * @see java.lang.Thread#run() java.lang.Thread#run()
+	 */
+	public abstract void run()
+	throws IOException;
+	}
---
+++ src/main/java/life/expert/common/io/SupplierIO.java
@@ -0,0 +1,51 @@
+package life.expert.common.io;
+//@Header@
+//--------------------------------------------------------------------------------
+//
+//                          archidoc  life.expert
+//                           wilmer 2019/01/30
+//
+//--------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+import java.io.IOException;
+
+
+
+
+/**
+ * Represents a supplier of results.
+ *
+ * <p>There is no requirement that a new or distinct result be returned each
+ * time the supplier is invoked.
+ *
+ * <p>This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #get()}.
+ *
+ * @param <T>
+ * 	the type of results supplied by this supplier
+ *
+ * @since 1.8
+ */
+@FunctionalInterface
+public interface SupplierIO<T> {
+
+
+
+/**
+ * Gets a result.
+ *
+ * @return a result
+ *
+ * @throws IOException
+ * 	the io exception
+ */
+T get() throws IOException;
+}
