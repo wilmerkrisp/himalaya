248237873cedae94e36c04e639038bc6bdc83b78
wilmer krisp <wilmerkrisp@yahoo.com>
2019-03-05 14:48:55 +0300

    added rxjava helper

--- src/main/java/life/expert/common/reactivex/LogUtils.java
+++ src/main/java/life/expert/common/reactivex/LogUtils.java
@@ -15,9 +15,45 @@ package life.expert.common.reactivex;
 
 
 
+import com.google.common.base.Strings;
+import com.google.common.flogger.FluentLogger;
+
+import java.util.function.Consumer;
+
+
+import io.reactivex.functions.Action;
+//import io.reactivex.functions.Consumer;
+
+
+
+
+
+
 public class LogUtils
 	{
+	private static final FluentLogger logger_ = FluentLogger.forEnclosingClass();
 	
 	
+	public static <E> Consumer<E> logAtInfoConsumer( String template )
+		{
+		
+		return ( o ) ->
+		{
+		logger_.atInfo()
+		       .log( Strings.lenientFormat( template , o ) );
+		};
+		}
+	
+	
+	
+	public static <E> Action logAtInfoRunnable( String template )
+		{
+		
+		return () ->
+		{
+		logger_.atInfo()
+		       .log( template );
+		};
+		}
 	
 	}
