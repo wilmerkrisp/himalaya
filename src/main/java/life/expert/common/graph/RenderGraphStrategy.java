81f09dc48560160e8da54277e8095815bbd8d1a9
wilmer krisp <wilmerkrisp@yahoo.com>
2019-07-10 16:51:31 +0300

    print all kind of graphs

---
+++ src/main/java/life/expert/common/graph/RenderGraphStrategy.java
@@ -0,0 +1,23 @@
+package life.expert.common.graph;
+//@Header@
+//--------------------------------------------------------------------------------
+//
+//                          himalaya  life.expert.common.graph
+//                           wilmer 2019/07/10
+//
+//--------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+public interface RenderGraphStrategy
+	{
+	
+	
+	
+	}
