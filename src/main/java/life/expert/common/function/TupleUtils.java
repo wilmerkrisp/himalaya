cf6920b267314cc9ea37d59807a26b63ac711928
wilmer krisp <wilmerkrisp@yahoo.com>
2019-08-01 11:55:13 +0300

    Tuple utils added

---
+++ src/main/java/life/expert/common/function/TupleUtils.java
@@ -0,0 +1,12 @@
+package life.expert.common.function;
+//@Header@
+//--------------------------------------------------------------------------------
+//
+//                          himalaya  life.expert.common.function
+//                           wilmer 2019/08/01
+//
+//--------------------------------------------------------------------------------
+
+public class TupleUtils
+	{
+	}
